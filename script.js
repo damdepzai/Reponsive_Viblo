7c0f7f3498ccce724d6e4f3d897e822105e6b09e
= <kubombo0007@gmail.com>
2019-11-10 13:13:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿ $(function(){
+	  //hiệu ứng khi kích vào class nutmenu
+ 	 $('.icon').click(function() {
+        $('.menu-left').addClass('ra');
+        return false;
+    });
+    //dong menu
+ 	 $('.fa-times').click(function(event) {
+        $('.menu-left').removeClass('ra');
+        return false;
+    });
+   $(window).scroll(function(){
+       if($(this).scrollTop() > 200){
+        $('.btnNut').fadeIn();
+       }
+       else{
+        $('.btnNut').fadeOut();
+       }
+   });
+    $('.btnNut').click(function(){
+        $('body,html').animate({scrollTop : 0},800);
+    });
+     
+
+    
+})  
+ 
